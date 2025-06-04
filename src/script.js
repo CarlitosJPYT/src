e8c3804e657e67d0df9d98a0a763863fd1f6ca4f
Carlitos <juanpana10ospina@gmail.com~>
2025-06-04 13:22:21 -0500

    Array

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo");
+const requiem = {
+    Introitus : ["Requiem", "Aeternam"],
+    Kyrie : "Eleison",
+    Sequentia : {
+        Dies : "Irae",
+        Tuba : "Mirum",
+        Rex : ["Tremendae", "Majestatis"],
+        Recordare : ["Jesu", "Pie"],
+        Confutatis : "Maledictus",
+        Lacrimosa : ["Dies", "Illa"],
+    }
+}
+console.log(requiem);
