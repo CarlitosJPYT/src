f51a3ce222912791b6845ca37a1f16c064dc0b35
Carlitos <juanpana10ospina@gmail.com~>
2025-06-07 09:57:45 -0500

    style

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const requiem = {
+    Introitus : ["Requiem", "Aeternam"],
+    Kyrie : "Eleison",
+    Sequentia : {
+        Dies : "Irae",
+        Tuba : "Mirum",
+        Rex : ["Tremendae", "Majestatis"],
+        Recordare : ["Jesu", "Pie"],
+        Confutatis : "Maledictus",
+        Lacrimosa : ["Dies", "Illa"],
+    },
+    Offertorium : {
+        Domine : ["Jesu", "Christe"],
+        Versus : ["Hostias", "Preces"]
+    },
+    Sanctus : {
+        Sanctus : ["Dominus", "Deus"],
+        Benedictus : ""
+    },
+    Agnus : "Dei",
+    Communio : ["Lux", "Aeterna"]
+}
+console.log(requiem);
